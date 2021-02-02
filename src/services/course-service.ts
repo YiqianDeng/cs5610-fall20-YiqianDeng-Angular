a0e60b4130fd6bddbf1c2e61395c1fd9adf7b8fb
YIQIAN DENG <yiqiandeng@YIQIANs-MBP.fios-router.home>
2021-02-02 02:59:03 -0500

    first commit

---
+++ src/services/course-service.ts
@@ -0,0 +1,38 @@
+import {Injectable} from '@angular/core';
+
+
+@Injectable()
+export class CourseService {
+  findAllCourses = () =>
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/yiqiandeng/courses`)
+      .then(response => response.json())
+
+  createCourse = () =>
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/yiqiandeng/courses`, {
+      method: 'POST',
+      body: JSON.stringify({title: 'New Course', editing: false}),
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+
+  deleteCourse = (id) =>
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/yiqiandeng/courses/${id}`, {
+      method: 'DELETE',
+    }).then(response => response.json())
+
+
+  updateCourse = (course) =>
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/yiqiandeng/courses/${course._id}`, {
+      method: 'PUT',
+      body: JSON.stringify(course),
+      headers: {
+        'content-type': 'application/json'
+      }
+    }).then(response => response.json())
+
+
+
+
+}
